e671d5626eea777690ba9cffd28fc68ac2cce635
Fred Rondina <fredrondina96@gmail.com>
2019-12-04 13:45:16 -0700

    Added additional partial functionality to valid_placement? method

--- lib/board.rb
+++ lib/board.rb
@@ -5,12 +5,15 @@ class Board
 	attr_reader :cells
 
 	def initialize
+		@rows = [*?A..?D] #["A","B","C","D"]
+		@columns = [*1..4].map { |n| n.to_s }
 		@cells = cell_create
 	end
-	
+
 	def cell_create
-		rows = ['A', 'B', 'C', 'D']
-                columns = [ '1', '2', '3', '4']
+
+		rows = @rows
+                columns = @columns
                 board_array = []
                 rows.each do |row|
                         columns.each do |column|
@@ -19,7 +22,7 @@ class Board
                 end
 		holder_array = []
 		board_array.each.with_index do |coordinate, index|
-			holder_array << board_array[index] = Cell.new(coordinate)	
+			holder_array << board_array[index] = Cell.new(coordinate)
 		end
 		hash ={}
 		holder_array.each.with_index do |cell, index|
@@ -27,7 +30,7 @@ class Board
 		end
 		return hash
 	end
-	
+
 	def valid_coordinate?(check_coordinate)
 		@cells.values.each do |cell|
 			if cell.coordinate == check_coordinate
@@ -37,8 +40,15 @@ class Board
 			return false
 	end
 
-			
+	def valid_placement?(ship, coordinates)
+		if ship.length != coordinates.count
+			return false
+		end
+		first = coordinates.first
+		row = first[0]
+		col = first[1]
+		return true
+	end
+
+
 end
-
-
-
